19d4ed01ae33bd07f09fa9e8692031bdd6c58b3b
okansolak <56310524+okansolak@users.noreply.github.com>
2024-12-04 03:12:56 +0300

    Update Aut.java

--- test/Aut.java
+++ test/Aut.java
@@ -1,15 +1,33 @@
 package test;
 import java.sql.*;
 
+/**
+ * Handles authentication for employees in the system.
+ * Uses password hashing and salting to ensure secure authentication.
+ * Manages the authentication process for employees, verifies their credentials and retrieves their details from the database.
+ */
 public class Aut 
 {
     private Databasefacade databasefacade;
-    
+    /**
+     * Constructor for the `Aut` class.
+     * @param databasefacade the database facade used for accessing employee and authentication data.
+     * @throws SQLException if there is an issue initializing the connection to the database
+     */
     public Aut(Databasefacade databasefacade) throws SQLException
     {
         this.databasefacade = databasefacade;
     }
-
+    /**
+     * Authenticates an employee using their username and password.
+     * Checks if the given username exists and verifies the password.
+     * Retrieves the hashed password and salt from the database.
+     * If the password matches the stored credentials, the corresponding `Employee` object is returned.
+     * @param username the username entered by the user
+     * @param password the password entered by the user
+     * @return An `Employee` object if authentication is successful; `null` otherwise.
+     * @throws SQLException If there is an issue with the database query
+     */
     public Employee authenticate(String username, String password) throws SQLException
     {
         if(!Databasefacade.usernameCheck(username))
